d314f06d8fd6b7f5a96f5e26eee95c3da5c633f1
Saifr72000 <saif.rana@me.com>
2025-08-12 17:23:32 +0200

    CORS issue fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://localhost:4173",
+      "https://ragtrader-frontend.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
